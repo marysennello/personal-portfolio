5dd3513575d32406271f993e9310728979c6582a
Mary Sennello <marysenn@uw.edu>
2023-05-10 18:46:21 -0700

    added components folder

---
+++ components/AboutSection.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="aboutMe page-section">
+   
+    <div class="flex flex-wrap lg:flex-nowrap justify-between items-center">
+
+      <div class="aboutMeInfo w-3/6 m-5">
+        <h2>About Me</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec dolor nulla. Pellentesque luctus ante eu dui ornare, viverra tincidunt neque gravida. Mauris tortor velit, feugiat a gravida sed, lobortis nec dui. Integer bibendum dictum sapien id egestas. Donec quis urna nibh. Ut tincidunt egestas tellus, at molestie mi. Nulla et augue id lectus eleifend tincidunt. Maecenas volutpat, sapien vitae vestibulum sodales, lorem erat condimentum augue, vitae scelerisque leo ligula eget felis. Vivamus turpis erat, maximus dictum rhoncus et, facilisis ac purus. Sed sed urna tempor metus molestie aliquam id id arcu. Donec vel placerat nibh. Nullam vitae velit non quam commodo convallis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi convallis, nisl nec aliquam consectetur, ante turpis blandit augue, eget pellentesque est felis sit amet odio. Curabitur fermentum rhoncus augue ut varius.
+        </p>
+        <p>
+          Nunc congue aliquet ex. Nunc maximus eleifend libero, id suscipit ligula. Aenean consequat pellentesque ipsum, sed tristique turpis pharetra sed. Sed nibh tortor, dignissim vitae posuere quis, luctus vel erat. Nunc ut maximus magna, et cursus ligula. Sed quis mollis libero. Sed ut leo sed risus pulvinar viverra at nec tellus. Praesent eu nunc euismod, dictum nibh et, condimentum quam. Vestibulum molestie porttitor eros. 
+        </p>
+      </div>
+
+      <div class="w-2/5 m-5">
+        <img src="../assets/images/mary-sennello-tree-lg.png" alt="portrait of Mary Sennello" class="object-fill rounded-lg">
+      </div>
+      
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  setup () {
+    
+
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
